8834a02c7600d743d8bf8450330a91c5a50803d2
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-12-30 16:20:38 +0000

    Versión 1 de la kata 4

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,7 +1,25 @@
 
+
 package kata4;
+
+import java.util.HashMap;
+import java.util.Map;
+import view.*;
+import model.*;
+
 
 public class Kata4 {
 
+   
+    public static void main(String[] args) {
+        
+        
+        MailListReader correos = new MailListReader();
+        MailHistogramBuilder histogramBuilder = new MailHistogramBuilder();
+        
+        
+        new HistogramDisplay(histogramBuilder.builder(correos.read("./email.txt"))).execute();
+        
+    }
     
 }
